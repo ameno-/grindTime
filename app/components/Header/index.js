2e00a128eab51608244abf9519a064d80d8c36d3
ameno- <ameno.osman13@gmail.com>
2017-04-01 01:56:03 -0700

    includes...

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -10,15 +10,12 @@ export default class Header extends Component {
   constructor(props) {
     super(props)
     this._deltaY = new Animated.Value(0);
-    this.state = {
-      canScroll: false
-    };
   }
   render() {
     return (
       <View style={styles.container}>
 
-          <View style={{backgroundColor: '#F99F51', height: 250, alignItems: 'center'}}>
+          <View style={{backgroundColor: '#E26D5A', height: 550, alignItems: 'center'}}>
             <Animated.View style={{
               transform: [
                 {
@@ -30,50 +27,25 @@ export default class Header extends Component {
                 {
                   scale: this._deltaY.interpolate({
                     inputRange: [-150, -150, 0, 0],
-                    outputRange: [0.5, 0.5, 1, 1]
+                    outputRange: [0.35, 0.35, 1, 1]
                   })
                 }
               ]
             }}>
-              <Text style={{fontSize: 80, color: 'white', marginTop: 80}}>25:00</Text>
+              <View style={{width: 150, height: 150, backgroundColor: '#F3FCF0', borderRadius: 75, marginTop: 50}} />
             </Animated.View>
           </View>
 
           <Interactable.View
             verticalOnly={true}
-            snapPoints={[{y: 0}, {y: -150, id: 'bottom'}]}
+            snapPoints={[{y: 0}, {y: -150}]}
             boundaries={{top: -150}}
-            onSnap={this.onSnap.bind(this)}
             animatedValueY={this._deltaY}>
-            <ScrollView
-              bounces={false}
-              canCancelContentTouches={this.state.canScroll}
-              onScroll={this.onScroll.bind(this)}
-              style={{left: 0, right: 0, height: Screen.height - 100, backgroundColor: 'white'}}>
-              <View style={styles.placeholder} />
-              <View style={styles.placeholder} />
-              <View style={styles.placeholder} />
-              <View style={styles.placeholder} />
-              <View style={styles.placeholder} />
-              <View style={styles.placeholder} />
-              <View style={styles.placeholder} />
-            </ScrollView>
+            <View style={{left: 0, right: 0, height: 650, backgroundColor: '#272727'}} />
           </Interactable.View>
 
       </View>
     );
-  }
-  onSnap(event) {
-    const { id } = event.nativeEvent;
-    if (id === 'bottom') {
-      this.setState({ canScroll: true });
-    }
-  }
-  onScroll(event) {
-    const { contentOffset } = event.nativeEvent;
-    if (contentOffset.y === 0) {
-      this.setState({ canScroll: false });
-    }
   }
 }
 
@@ -81,12 +53,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-  },
-  placeholder: {
-    backgroundColor: '#CD1931',
-    flex: 1,
-    height: 120,
-    marginHorizontal: 20,
-    marginTop: 20
   }
 });
